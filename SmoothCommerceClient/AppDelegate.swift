3f37a1f86b0aa36358674ab87e93b8b973985815
MAC <mac@MACs-MacBook-Pro.local>
2018-07-04 13:42:06 -0400

    Initial code push

--- SmoothCommerceClient/AppDelegate.swift
+++ SmoothCommerceClient/AppDelegate.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 import CoreData
+import SmoothCommerceFramework
+
+let logger = SmoothCommerceDebug()
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        logger.setup(isDebug: true)
         return true
     }
 
